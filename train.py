fadaa863b171ab0703458207f722530e9967da9d
Sherlock <you@example.com>
2022-05-04 16:42:32 +0800

    update args

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from utils import Trainer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_file", type=str, default='data')
-    parser.add_argument('out_model_file', type=str, default='parameters.pt')
+    parser.add_argument("data_file", nargs='?', type=str, default='data')
+    parser.add_argument('out_model_file', nargs='?', type=str, default='parameters.pt')
     parser.add_argument('in_model_file', nargs='?', type=str, default=None)
     parser.add_argument('epochs', nargs='?', type=int, default=5)
     args = parser.parse_args()
